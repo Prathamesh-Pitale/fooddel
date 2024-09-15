ecfc9a77810fb72f6c6a5dde47c94cf245dc649f
Prathamesh-Pitale <pitaleprathamesh89@gmail.com>
2024-09-15 23:13:40 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -52,7 +52,7 @@ const StoreContextProvider = (props) =>{
         for(const item in cartItems)
         {
             if(cartItems[item]>0){
-            let itemInfo = food_list.find((product)=>product._id === item);
+            //let itemInfo = food_list.find((product)=>product._id === item);
             total += 1;
         }
         }
