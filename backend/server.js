08bc04394cbb0219230fcfe9d2a2a6e1e3178cb8
Prathamesh-Pitale <pitaleprathamesh89@gmail.com>
2025-02-23 11:20:50 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,9 @@ const allowedOrigins = [
       }
     },
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type,Authorization"
+    allowedHeaders: "Content-Type,Authorization,token",
+    credentials: true
+
   }));
 
 // app.use(cors({
