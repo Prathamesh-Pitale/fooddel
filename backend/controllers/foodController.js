23efb49cf6d2e38c27cae7749c5c14742e1a433f
Prathamesh-Pitale <pitaleprathamesh89@gmail.com>
2025-02-23 11:46:04 +0530

    typeerror

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -85,7 +85,7 @@ const getPublicIdFromUrl = (imageUrl) => {
     }
 };
 
-cconst removeFood = async (req, res) => {
+const removeFood = async (req, res) => {
     try {
         const food = await foodModel.findById(req.body.id);
         if (!food) {
