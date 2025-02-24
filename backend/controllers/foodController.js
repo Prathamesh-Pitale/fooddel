bb26e298d08252e6615c9337deec3d2c99aa5dcb
Prathamesh-Pitale <pitaleprathamesh89@gmail.com>
2025-02-24 23:11:45 +0530

    remfood update

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -78,7 +78,7 @@ const getPublicIdFromUrl = (imageUrl) => {
         // Remove all extensions (.png, .jpeg, .jpg, etc.)
         const fileName = fileNameWithExt.replace(/\.[^.]+$/, '').replace(/\.[^.]+$/, '');
 
-        return `${fileName}`;
+        return `${folder}/${fileName}`;
     } catch (error) {
         console.error("❌ Error extracting public ID:", error);
         return null;
